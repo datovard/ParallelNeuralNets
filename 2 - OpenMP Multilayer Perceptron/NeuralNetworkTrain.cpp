12f018cc453fbc8ce074fa1bba13c9cc1d92250d
datovard <abadtovar@gmail.com>
2021-12-09 01:05:15 -0500

    Fixes in file readings and main file for OpenMP

---
+++ 2 - OpenMP Multilayer Perceptron/NeuralNetworkTrain.cpp
@@ -0,0 +1,71 @@
+// main.cpp
+ 
+// don't forget to include out neural network
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+#include <sstream>
+#include "NeuralNetwork.hpp"
+#include "../util/FileReading.cpp"
+
+using namespace std;
+
+// Weights file name
+const string model_fn = "output/model-neural-network";
+
+// Report file name
+const string report_fn = "output/training-report.dat";
+
+// Number of training samples
+int nTraining = 60000;
+
+// File stream to write down a report
+ofstream report;
+
+int main(int argc, char *argv[])
+{
+    createTrainTestCSVs(60000, 10000, "2 - OpenMP Multilayer Perceptron");
+    struct timeval tval_before, tval_after, tval_result;
+
+    cout << "Setting up...\n"; 
+    report.open(report_fn.c_str(), ios::out);
+    bool **in_dat = (bool **) malloc( (nTraining+1) * sizeof(bool*) );
+    bool **out_dat = (bool **) malloc( (nTraining+1) * sizeof(bool*) );
+
+    int input_cols = ReadCSV("input/training_input.csv", in_dat, nTraining);
+    int labels_cols =  ReadCSV("input/training_labels.csv", out_dat, nTraining);
+    
+    int num_threads [] = {8}; 
+
+    cout << "Training...\n"; 
+    for(int i = 0; i < 1; i++){
+        report << "Number of threads: " << num_threads[i] << "\n";
+        for(int j = 0; j < 1; j++){
+            gettimeofday(&tval_before, NULL);
+            
+            NeuralNetwork net(input_cols, labels_cols, num_threads[i]);
+
+            int sample = 0;
+            for( sample; sample < nTraining; sample++ ){
+                net.updateInput(in_dat[sample]);
+                net.updateExpectedOutput(out_dat[sample]);
+
+                int nIterations = net.train();
+            }
+
+            gettimeofday(&tval_after, NULL);
+            timersub(&tval_after, &tval_before, &tval_result);
+
+            net.write_matrix(model_fn + "_" + to_string(num_threads[i]) + "-" + to_string(j+1) + ".dat");
+            report << "Sample " << sample << ", Error = " << net.calculateErrors();
+            report << ", Time elapsed training: " << ((long int)tval_result.tv_sec) << "." << ((long int)tval_result.tv_usec) << " seconds" << endl ;
+        }
+
+        report << endl;
+    }
+
+    report.close();
+    return 0;
+}
---
+++ util/FileReading.cpp
@@ -0,0 +1,124 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+inline bool fileExists (const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }   
+}
+
+void createCSV(int numberSamples, std::string inputs, std::string labels, std::string outputFolder, std::string outputSuffix){
+  const std::string testing_image_fn = inputs;
+  const std::string testing_label_fn = labels;
+  const std::string testing_inputs_out = "../" + outputFolder + outputSuffix + "_input.csv";
+  const std::string testing_labels_out = "../" + outputFolder + outputSuffix + "_labels.csv";
+
+  if( fileExists(testing_inputs_out) && fileExists(testing_labels_out) ){
+    return;
+  }
+
+  std::ifstream testing_image, testing_label;
+  std::ofstream testing_inputs, testing_labels;
+
+  testing_inputs.open(testing_inputs_out.c_str(), std::ios::out);
+  testing_labels.open(testing_labels_out.c_str(), std::ios::out);
+  testing_image.open(testing_image_fn.c_str(), std::ios::in | std::ios::binary); // Binary image file
+  testing_label.open(testing_label_fn.c_str(), std::ios::in | std::ios::binary ); // Binary label file
+
+  const int width = 28;
+  const int height = 28;
+  const int n3 = 10;
+
+  // Reading file headers
+  char number;
+  for (int i = 1; i <= 16; ++i) {
+      testing_image.read(&number, sizeof(char));
+  }
+  for (int i = 1; i <= 8; ++i) {
+      testing_label.read(&number, sizeof(char));
+  }
+
+  for (int sample = 1; sample <= numberSamples; ++sample) {
+        // Reading testing images
+        for (int j = 1; j <= height; ++j) {
+            for (int i = 1; i <= width; ++i) {
+                testing_image.read(&number, sizeof(char));
+                int pos = i + (j - 1) * width;
+                testing_inputs << ((number == 0)? 0: 1);
+                if( pos < height * width )
+                    testing_inputs << ",";
+            }
+        }
+        testing_inputs << "\n";
+
+        // Reading testing labels
+        testing_label.read(&number, sizeof(char));
+        for (int i = 1; i <= n3; ++i) {
+            testing_labels << (( i == number + 1 )? 1: 0);
+            if( i < n3 )
+            testing_labels << ",";
+        }
+        testing_labels << "\n";
+    }
+
+    testing_image.close();
+    testing_label.close();
+    testing_inputs.close();
+    testing_labels.close();
+}
+
+
+void createTrainTestCSVs(int trainingSamples, int testingSamples, std::string outputFolder){
+    const std::string training_image_fn = "../data/train-images.idx3-ubyte";
+    const std::string training_label_fn = "../data/train-labels.idx1-ubyte";
+
+    const std::string testing_image_fn = "../data/t10k-images.idx3-ubyte";
+    const std::string testing_label_fn = "../data/t10k-labels.idx1-ubyte";
+    const std::string output_fn = outputFolder + "/input/";
+
+    createCSV(trainingSamples, training_image_fn, training_label_fn, output_fn, "training");
+    createCSV(testingSamples, testing_image_fn, testing_label_fn, output_fn, "testing");
+}
+
+int ReadCSV(std::string filename, bool** in_dat, int nTraining)
+{
+    std::ifstream file(filename);
+    std::string line, word;
+
+    // determine number of columns in file
+    getline(file, line, '\n');
+    std::stringstream ss(line);
+    std::vector<bool> parsed_vec;
+
+    while (getline(ss, word, ',')) {
+        parsed_vec.push_back( (bool) (std::stoi(&word[0]))) ;
+    }
+    uint cols = parsed_vec.size();
+    in_dat[0] = new bool[cols];
+    for(int i = 0; i < cols; i++){
+        in_dat[0][i] = parsed_vec[i];
+    }
+ 
+    // read the file
+    int i = 1;
+    if (file.is_open()) {
+        while (getline(file, line, '\n') && i < nTraining) {
+            std::stringstream ss(line);
+
+            in_dat[i] = new bool[cols];
+            uint j = 0;
+            while (getline(ss, word, ',')) {
+                in_dat[i][j] = (bool) std::stoi(&word[0]);
+                j++;
+            }
+            i++;
+        }
+    }
+    return cols;
+}
