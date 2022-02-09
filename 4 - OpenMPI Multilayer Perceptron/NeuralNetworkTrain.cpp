800e0f21d33c94e5360bd53d67ee31f2b08a3711
datovard <abadtovar@gmail.com>
2022-02-09 01:14:08 -0500

    Final changes

--- 4 - OpenMPI Multilayer Perceptron/NeuralNetworkTrain.cpp
+++ 4 - OpenMPI Multilayer Perceptron/NeuralNetworkTrain.cpp
@@ -1,3 +1,14 @@
+// ejecutar con mpirun -np 4 mpi_test
+// mpicc OpenMPI-Test.c -o OpenMPI-Test && mpirun -np 6 --hostfile mpi-hosts OpenMPI-Test
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <mpi.h>
+#include "FileReading.cpp"
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -9,8 +20,6 @@
 #include <set>
 #include <iterator>
 #include <algorithm>
-#include <omp.h>
-#include "../util/FileReading.cpp"
 
 using namespace std;
 
@@ -35,6 +44,12 @@ const int nTraining = 60000;
 // Number of testing samples
 const int nTesting = 10000;
 
+// Number of distributed hosts
+const int nHosts = 18;
+
+// Size of each batch on each host
+const int batch_size = nTraining / (nHosts - 1);
+
 // Image size in MNIST database
 const int width = 28;
 const int height = 28;
@@ -51,7 +66,7 @@ const int n1 = width * height; // = 784, without bias neuron
 const int n2 = 128; 
 const int n3 = 10; // Ten classes: 0 - 9
 const int iterations = 512;
-const int batch_size = 32;
+const int epochs = 1;
 const double learning_rate = 1e-3;
 const double momentum = 0.9;
 const double epsilon = 1e-3;
@@ -60,11 +75,13 @@ const double epsilon = 1e-3;
 double *w1, *delta1, *out1;
 double *original_w1;
 double *final_w1;
+double *difference_w1;
 
 // From layer 2 to layer 3. Or; Hidden layer - Output layer
 double *w2, *delta2, *in2, *out2, *theta2;
 double *original_w2;
 double *final_w2;
+double *difference_w2;
 
 // Layer 3 - Output layer
 double *in3, *out3, *theta3;
@@ -80,53 +97,36 @@ ofstream report_test;
 double **in_dat, **out_dat;
 double **in_test, **out_test;
 
-// +-----------------------------------+
-// | Memory allocation for the network |
-// +-----------------------------------+
-
 void copy_array(const double * original, double * copy, int size) {
-  #pragma omp parallel for
   for (int i = 0; i < size; i++) {
     copy[i] = original[i];
 	}
 }
 
 void clean_array(double * array, int size) {
-  #pragma omp parallel for
   for (int i = 0; i < size; i++) {
     array[i] = 0;
 	}
 }
 
 void add_cumulative_array(const double * original, double * cumulative, int size) {
-  #pragma omp parallel for
   for (int i = 0; i < size; i++) {
     cumulative[i] += original[i];
 	}
 }
 
 void get_weights_difference(double * original, double * weights, double * finals, int size, int model_amount){
-  #pragma omp parallel for
   for(int i = 0; i < size; i++){
     double change = (original[i] - weights[i] <= 0)? (weights[i] - original[i]): -1*(original[i] - weights[i]);
     finals[i] += change / model_amount;
   }
 }
 
-bool arrays_are_equal(double * arr1, double * arr2, int size){
-  for( int i = 0; i < size; i++ ){
-    if(abs(final_w2[i] - w2[i]) > 0.00001){ 
-      return false; 
-    }
-  }
-
-  return true;
-}
-
-void init_array() {
+void init_arrays() {
   // Layer 1 - Layer 2 = Input layer - Hidden layer
   w1 = new double[n1*n2];
   original_w1 = new double[n1*n2];
+  difference_w1 = new double[n1*n2];
   final_w1 = new double[n1*n2];
   delta1 = new double[n1*n2];
   out1 = new double[n1];
@@ -134,6 +134,7 @@ void init_array() {
   // Layer 2 - Layer 3 = Hidden layer - Output layer
   w2 = new double[n2*n3];
   original_w2 = new double[n2*n3];
+  difference_w2 = new double[n2*n3];
   final_w2 = new double[n2*n3];
   delta2 = new double[n2 * n3];
   in2 = new double [n2];
@@ -146,23 +147,19 @@ void init_array() {
   theta3 = new double [n3];
 
   expected = new double [n3];
+}
 
-   // Initialization for weights from Input layer to Hidden layer
-  #pragma omp parallel for
+void fill_weights(){
+  // Initialization for weights from Input layer to Hidden layer
   for (int i = 0; i < n1 * n2; i++) {
     original_w1[i] = ((rand() % 2 == 1)? -1: 1) * (double)(rand() % 6) / 10.0;
 	}
 	
 	// Initialization for weights from Hidden layer to Output layer
-  #pragma omp parallel for
   for (int i = 0; i < n2 * n3; i++) {
     original_w2[i] = ((rand() % 2 == 1)? -1: 1) * (double)(rand() % 10 + 1) / (10.0 * n3);
   }
 }
-
-// +------------------+
-// | Sigmoid function |
-// +------------------+
 
 double sigmoid(double x) {
   return 1.0 / (1.0 + exp(-x));
@@ -186,10 +183,6 @@ void adjust_weights(const double* thetas, const double* outputs, double* deltas,
     }
 	}
 }
-
-// +------------------------------+
-// | Forward process - Perceptron |
-// +------------------------------+
 
 void perceptron() {
   clean_array(in2, n2);
@@ -229,10 +222,6 @@ void final_perceptron() {
 	}
 }
 
-// +---------------+
-// | Norm L2 error |
-// +---------------+
-
 double square_error(){
   double res = 0.0;
   for (int i = 1; i <= n3; ++i) {
@@ -268,10 +257,6 @@ void back_propagation() {
   adjust_weights(theta2, out1, delta1, w1, n1, n2, learning_rate, momentum);
 }
 
-// +-------------------------------------------------+
-// | Learning process: Perceptron - Back propagation |
-// +-------------------------------------------------+
-
 int learning_process() {
   clean_array(delta1, n1 * n2);
   clean_array(delta2, n2 * n3);
@@ -288,10 +273,6 @@ int learning_process() {
   return i;
 }
 
-// +--------------------------------------------------------------+
-// | Reading input - gray scale image and the corresponding label |
-// +--------------------------------------------------------------+
-
 void input(const double* image, const double* label) {
   // Reading image
   for(int i = 0; i < height * width; i++){
@@ -304,16 +285,12 @@ void input(const double* image, const double* label) {
   }
 }
 
-// +------------------------+
-// | Saving weights to file |
-// +------------------------+
-
 void write_matrix(string file_name) {
   ofstream file(file_name.c_str(), ios::out);
 
   // Input layer - Hidden layer
   for (int pos = 0; pos < n1 * n2; pos++) {
-		file << w1[pos] << " ";
+		file << final_w1[pos] << " ";
 		
     if( pos % width == n2 )
       file << endl;
@@ -321,7 +298,7 @@ void write_matrix(string file_name) {
 	
 	// Hidden layer - Output layer
   for (int pos = 0; pos < n2 * n3; pos++) {
-		file << w2[pos] << " ";
+		file << final_w2[pos] << " ";
 		
     if( pos % width == n3 )
       file << endl;
@@ -331,7 +308,6 @@ void write_matrix(string file_name) {
 }
 
 void read_dataset(){
-  cout << "Setting up...\n"; 
   report_train.open(report_train_fn.c_str(), ios::out);
   report_test.open(report_test_fn.c_str(), ios::out);
   in_dat = (double **) malloc( (nTraining+1) * sizeof(double*) );
@@ -345,87 +321,7 @@ void read_dataset(){
   ReadCSVOnDouble("input/testing_labels.csv", out_test, nTesting);
 }
 
-// +--------------+
-// | Main Program |
-// +--------------+
-
-int main(int argc, char *argv[]) {
-  omp_set_num_threads(12);
-  createTrainTestCSVs(60000, 10000, "4 - OpenMPI Multilayer Perceptron");
-  struct timeval tval_before, tval_after, tval_result;
-
-  read_dataset();
-
-  // Neural Network Initialization
-  init_array();
-
-  int batch_size = 100;
-  int model_amount = 3;
-  int epochs = 1;
-
-  copy_array(original_w1, final_w1, n1 * n2);
-  copy_array(original_w2, final_w2, n2 * n3);
-  
-  for(int model = 0; model < model_amount; model++){
-    copy_array(original_w1, w1, n1 * n2);
-    copy_array(original_w2, w2, n2 * n3);
-
-    for(int epoch = 1; epoch <= epochs; epoch++){
-      for(int sample = 0; sample < batch_size; sample++){      
-        // Getting (image, label)
-        input(in_dat[sample + (model * batch_size)], out_dat[sample + (model * batch_size)]);
-
-        // Learning process: Perceptron (Forward procedure) - Back propagation
-        int nIterations = learning_process();
-
-        // Write down the squared error
-        if(sample % 500 == 0){
-          report_train << "Sample " << sample << ": No. iterations = " << nIterations << ", Error = " << square_error() << endl;
-        }
-      }
-    }
-
-    int nCorrect = 0, label = 0;;
-    for (int sample = 0; sample < nTesting; sample++) {
-      // Getting (image, label)
-      input(in_test[sample], out_test[sample]);
-
-      label = 0;
-      for( label; label < 9; label++){
-        if(out_test[sample][label]) break;
-      }
-    
-      // Classification - Perceptron procedure
-      perceptron();
-        
-      /// Prediction
-      int predict = 0;
-      for (int i = 1; i < n3; i++) {
-        if (out3[i] > out3[predict]) {
-          predict = i;
-        }
-      }
-
-      // Write down the classification result and the squared error
-      double error = square_error();
-    
-      if (label == predict) {
-        ++nCorrect;
-      }
-    }
-
-    // Summary
-    double accuracy = (double)(nCorrect) / nTesting * 100.0;
-    cout << "Number of correct samples: " << nCorrect << " / " << nTesting << endl;
-    printf("Accuracy: %0.2lf\n", accuracy);
-    
-    report_test << "Number of correct samples: " << nCorrect << " / " << nTesting << endl;
-    report_test << "Accuracy: " << accuracy << endl;
-
-    get_weights_difference( original_w1, w1, final_w1, n1 * n2, model_amount);
-    get_weights_difference( original_w2, w2, final_w2, n2 * n3, model_amount);
-  }
-
+void test_model(int node){
   int nCorrect = 0, label = 0;;
   for (int sample = 0; sample < nTesting; sample++) {
     // Getting (image, label)
@@ -457,13 +353,89 @@ int main(int argc, char *argv[]) {
 
   // Summary
   double accuracy = (double)(nCorrect) / nTesting * 100.0;
+  cout << "Node " << node << " test: \n";
   cout << "Number of correct samples: " << nCorrect << " / " << nTesting << endl;
   printf("Accuracy: %0.2lf\n", accuracy);
   
   report_test << "Number of correct samples: " << nCorrect << " / " << nTesting << endl;
-  report_test << "Accuracy: " << accuracy << endl;
+  report_test << "Accuracy: " << accuracy << "\n" << endl;
+}
 
-  report_train.close();
-  report_test.close();
+int main (int argc, char *argv[])
+{
+  int i, tag=1, tasks, iam;
+  int start = 0, end = 0;
+  MPI_Status status;
+
+  struct timeval tval_before, tval_after, tval_result;
+  gettimeofday(&tval_before, NULL);
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &tasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &iam);
+    char hostname[HOST_NAME_MAX];
+    char username[LOGIN_NAME_MAX];
+    gethostname(hostname, HOST_NAME_MAX);
+    getlogin_r(username, LOGIN_NAME_MAX);
+
+    read_dataset();
+
+    init_arrays();
+
+    if(iam == 0){
+      fill_weights();
+
+      copy_array(original_w1, final_w1, n1 * n2);
+      copy_array(original_w2, final_w2, n2 * n3);
+    } else {
+      start = (iam-1) * batch_size;
+      end = ((iam-1) * batch_size) + batch_size - 1;
+    }
+
+    MPI_Bcast(original_w1, n1*n2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(original_w2, n2*n3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    if(iam != 0){
+      copy_array(original_w1, w1, n1 * n2);
+      copy_array(original_w2, w2, n2 * n3);
+
+      for(int epoch = 1; epoch <= epochs; epoch++){
+        for(int sample = start; sample <= end; sample++){
+          // Getting (image, label)
+          input(in_dat[sample], out_dat[sample]);
+
+          // Learning process: Perceptron (Forward procedure) - Back propagation
+          int nIterations = learning_process();
+        }
+      }
+      
+      copy_array(w1, final_w1, n1 * n2);
+      copy_array(w2, final_w2, n2 * n3);
+      get_weights_difference(original_w1, w1, difference_w1, n1 * n2, nHosts - 1);
+      get_weights_difference(original_w2, w2, difference_w2, n2 * n3, nHosts - 1);
+
+      MPI_Send(difference_w1, n1 * n2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+      MPI_Send(difference_w2, n2 * n3, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+    } else {
+      for(int i = 1; i <= nHosts - 1; i++){
+        MPI_Recv(difference_w1, n1 * n2, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
+        MPI_Recv(difference_w2, n2 * n3, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
+
+        add_cumulative_array(difference_w1, final_w1, n1*n2);
+        add_cumulative_array(difference_w2, final_w2, n2*n3);
+      }
+
+      gettimeofday(&tval_after, NULL);
+      timersub(&tval_after, &tval_before, &tval_result);
+
+      cout << "Time elapsed testing: " << ((long int)tval_result.tv_sec) << "." << ((long int)tval_result.tv_usec) << " seconds" << endl;
+      cout << endl;
+
+      test_model(iam);
+      write_matrix(model_fn);
+    }
+
+  MPI_Finalize();
+  
   return 0;
 }
